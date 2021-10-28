ce14ebb760803709d40e401a9f4fcb0c4f99b9aa
Guit <ggojard@gmail.com>
2021-10-28 16:43:10 +0200

    adapt styles

--- starter/components/Navbar.vue
+++ starter/components/Navbar.vue
@@ -1,26 +1,24 @@
 <template>
-<div class="flex justify-between ml-6 mr-6 mt-4">
+<div class="flex justify-between ml-6 mr-6 mt-4 font-bold text-yellow-900">
   <nuxt-link to="/">
     <span class="emoji">
       <img src="~/assets/logo.png" class="logo" height="100" width="100" />
     </span>
   </nuxt-link>
-  <span class="flex items-center font-bold text-yellow-900">
-      So'Ceramic - Poteries Artisanales
-  </span>  
+
+  <div>So'Ceramic - Poteries Artisanales</div>
+  
   <nuxt-link to="/">
-    <span class="flex items-center font-semibold text-yellow-900">
-      Catalogue
-    </span>
+    <span class="flex items-center font-semibold hover:text-yellow-700">Catalogue</span>
   </nuxt-link>
+
   <nuxt-link to="/about">
-    <span class="flex items-center font-semibold text-yellow-900 hover:text-yellow-800">
-      A propos
-    </span>
+    <span class="flex items-center font-semibold hover:text-yellow-700">A propos</span>
   </nuxt-link>
+  
   <button class="snipcart-checkout flex items-center">
     <Cart />
-    <span class="snipcart-total-price ml-3 font-semibold text-sm text-yellow-900 hover:text-yellow-800"></span>
+    <span class="snipcart-total-price ml-3 font-semibold text-sm text-yellow-900"></span>
   </button>
 </div>
 </template>
