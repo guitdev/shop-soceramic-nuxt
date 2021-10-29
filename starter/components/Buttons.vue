e0d4dae0599330b98ae03f52dc16a8556c5f6c73
Guit <ggojard@gmail.com>
2021-10-29 15:43:49 +0200

    check if prd in category

--- starter/components/Buttons.vue
+++ starter/components/Buttons.vue
@@ -5,9 +5,9 @@
   </div>
   <div v-else>
     <div class="container flex flex-wrap mx-auto ml-4 mr-6 mt-8">
-      <nuxt-link to="/" class="bg-soceramicTxt hover:bg-gray-100 text-soceramicBg hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full shadow">Tout</nuxt-link>
+      <nuxt-link to="/" class="bg-white hover:bg-gray-100 text-soceramicTxt  hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full">Tout</nuxt-link>
 
-      <nuxt-link v-for="category in categories" :to="`/categories/${category.slug}`" :key="category.id" class="bg-white hover:bg-gray-100 text-soceramicTxt  hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full shadow">
+      <nuxt-link v-for="category in categories" :to="`/categories/${category.slug}`" :key="category.id" class="bg-white hover:bg-gray-100 text-soceramicTxt  hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full">
         {{ category.name }}
       </nuxt-link>
     </div>
--- starter/pages/about.vue
+++ starter/pages/about.vue
@@ -1,7 +1,7 @@
 <template>
     <div text-soceramicTxt>
-      <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">
-        <nuxt-link to="/" class="hover:bg-gray-100 hover:text-yellow-700 font-semibold py-2 px-4 ml-2 rounded shadow">Retour au catalogue</nuxt-link>
+      <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">        
+        <nuxt-link to="/" class="bg-white hover:bg-gray-100 text-soceramicTxt  hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full">Retour au catalogue</nuxt-link>
       </div>
       <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">
         <h4 class="mt-1 font-semibold text-base leading-tight truncate ">
@@ -11,6 +11,7 @@
           Ici je vends une partie de mes produits. Fabrication artisanale française. Tous les produits sont des pièces faconnées à la main en quantité raisonée. SO'ceramic offre une gamme de céramiques épurées et contemporaines.
           </div>
       </div>
+
       <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">
         <h4 class="mt-1 font-semibold text-base leading-tight truncate ">
           2021 l’année du changement
@@ -37,6 +38,66 @@
         <div class="mt-1 text-sm">
           Les mains dedans 🖐
         </div>
+      </div>
+
+      <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">
+        <h4 class="mt-1 font-semibold text-base leading-tight truncate ">
+          2021 l’année du changement
+        </h4>
+        <div class="mt-1 text-sm">
+          Du renouveau et un retour à la terre !
+          Tout à commencez dans le cabanon, au fond de mon jardin ... ⛺
+        </div>
+      </div>
+
+      <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+        <h4 class="mt-1 font-semibold text-base leading-tight truncate">
+          Une formation en ligne
+        </h4>
+        <div class="mt-1 text-sm">
+          Derniere mon écran 💻
+        </div>
+      </div>
+
+      <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+        <h4 class="mt-1 font-semibold text-base leading-tight truncate">
+          Une formation en vraie
+        </h4>
+        <div class="mt-1 text-sm">
+          Les mains dedans 🖐
+        </div>
+      </div>
+
+      <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">
+        <h4 class="mt-1 font-semibold text-base leading-tight truncate ">
+          2021 l’année du changement
+        </h4>
+        <div class="mt-1 text-sm">
+          Du renouveau et un retour à la terre !
+          Tout à commencez dans le cabanon, au fond de mon jardin ... ⛺
+        </div>
+      </div>
+
+      <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+        <h4 class="mt-1 font-semibold text-base leading-tight truncate">
+          Une formation en ligne
+        </h4>
+        <div class="mt-1 text-sm">
+          Derniere mon écran 💻
+        </div>
+      </div>
+
+      <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+        <h4 class="mt-1 font-semibold text-base leading-tight truncate">
+          Une formation en vraie
+        </h4>
+        <div class="mt-1 text-sm">
+          Les mains dedans 🖐
+        </div>
+      </div>
+
+      <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">        
+        <nuxt-link to="/" class="bg-white hover:bg-gray-100 text-soceramicTxt  hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full">Retour au catalogue</nuxt-link>
       </div>
     </div>
 </template>
