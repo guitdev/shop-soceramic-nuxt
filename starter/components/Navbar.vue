b8f573e0c90704ea9b50fd03326b47067d6bef26
Guit <ggojard@gmail.com>
2021-10-29 12:18:30 +0200

    change navabr txt

--- starter/components/Navbar.vue
+++ starter/components/Navbar.vue
@@ -2,11 +2,10 @@
 <div class="flex items-center justify-between ml-6 mr-6 mt-4 font-bold text-soceramicTxt">
   <nuxt-link to="/">
     <span class="emoji">
-      <img src="~/assets/soceramic-logo_text.png" class="logo" width="200px" />
+      <img src="~/assets/soceramic-logo_text.png" class="logo" width="150px" />
     </span>
-    <div>Poteries Artisanales</div>
   </nuxt-link>
-     
+  <div class="flex items-center m-6 text-xs">Céramiques et Poteries Artisanales</div>
   <button class="snipcart-checkout flex items-center">
     <Cart />
     <span class="snipcart-total-price ml-3 font-semibold text-sm text-soceramicTxt"></span>
