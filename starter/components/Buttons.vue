87ca4b260f596f61ecfecf4bd493ffc364fd58fe
Guit <ggojard@gmail.com>
2021-10-29 16:29:51 +0200

    remove check if prd in category

--- starter/components/Buttons.vue
+++ starter/components/Buttons.vue
@@ -6,15 +6,9 @@
   <div v-else>
     <div class="container flex flex-wrap mx-auto ml-4 mr-6 mt-8">
       <nuxt-link to="/" class="bg-white hover:bg-gray-100 text-soceramicTxt  hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full">Tout</nuxt-link>
-
-      <span v-for="category in categories">
-        <span v-if="category.products !== null">
-          <nuxt-link :to="`/categories/${category.slug}`" :key="category.id" class="bg-white hover:bg-gray-100 text-soceramicTxt  hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full">
-            {{ category.name }}
-          </nuxt-link>
-        </span>
-      </span>
-
+      <nuxt-link v-for="category in categories" :to="`/categories/${category.slug}`" :key="category.id" class="bg-white hover:bg-gray-100 text-soceramicTxt  hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full">
+          {{ category.name }}
+      </nuxt-link>
     </div>
 
   </div>
