2e2f3428b51873dc62aadd2afd57bdd4721919ff
Guillaume Gojard <ggojard@gmail.com>
2021-10-31 12:40:19 +0100

    test menu2. add contact

--- starter/components/Navbar.vue
+++ starter/components/Navbar.vue
@@ -1,11 +1,51 @@
 <template>
+
+<base-nav type="primary"
+        v-model="showMenu2"
+        >
+  <div slot="brand" class="navbar-brand" href="#">
+    <nuxt-link to="/">
+      <span class="emoji">
+        <img src="~/assets/soceramic-logo.png" class="logo" width="200px" />
+        </span>
+    </nuxt-link>
+  </div>
+
+  <ul class="navbar-nav">
+     <li class="nav-item active">
+       <nuxt-link to="/" class="nav-link">ACCUEIL <span class="sr-only">(current)</span></nuxt-link>
+     </li>
+     <li class="nav-item">
+        <nuxt-link to="/about" class="nav-link">A PROPOS</nuxt-link>
+     </li>
+     <li class="nav-item">
+        <nuxt-link to="/catalog" class="nav-link">CATALOGUE</nuxt-link>
+     </li>
+       <base-dropdown tag="li"
+                      title="CATALOGUE"
+                      title-tag="a"
+                      title-classes="nav-link"
+                      class="nav-item">
+
+        <span slot="title">
+          CATALOGUE
+        </span>
+        <a class="dropdown-item" href="/catalog">Tout</a>
+        <a class="dropdown-item" href="/catalog">Assiettes</a>
+        <a class="dropdown-item" href="/catalog">Taases</a>
+     </base-dropdown>
+
+     <li class="nav-item">
+        <nuxt-link to="/contact" class="nav-link">CONTACT</nuxt-link>
+     </li>
+
+  </ul>
+</base-nav>
+
+---
+
 <nav class="flex items-center justify-between flex-wrap p-6">
     <div class="flex items-center flex-shrink-0  mr-6">
-      <nuxt-link to="/">
-        <span class="emoji">
-          <img src="~/assets/soceramic-logo.png" class="logo" width="150px" />
-        </span>
-      </nuxt-link>
     </div>
 
     <nuxt-link to="/">
@@ -27,9 +67,11 @@
 
 <script>
 import Cart from "./icons/cart.vue"
+import {BaseNav} from '@/components'
 
 export default {
   components: {
+    BaseNav,
     Cart
   },
   data: function() {
---
+++ starter/pages/contact.vue
@@ -0,0 +1,108 @@
+<template>
+    <div text-soceramicTxt>
+      <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">
+        <nuxt-link to="/catalog" class="bg-white hover:bg-gray-100 hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full">Consulter le catalogue</nuxt-link>
+      </div>
+
+      <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">
+        <h4 class="mt-1 font-semibold leading-tight truncate ">
+          2021 l’année du changement
+        </h4>
+        <div class="mt-1 text-sm">
+          Du renouveau et un retour à la terre !
+          Tout à commencez dans le cabanon, au fond de mon jardin ... ⛺
+        </div>
+      </div>
+
+      <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+        <h4 class="mt-1 font-semibold  leading-tight truncate">
+          Une formation en ligne
+        </h4>
+        <div class="mt-1 text-sm">
+          Derniere mon écran 💻
+        </div>
+      </div>
+
+      <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+        <h4 class="mt-1 font-semibold  leading-tight truncate">
+          Une formation en vraie
+        </h4>
+        <div class="mt-1 text-sm">
+          Les mains dedans 🖐
+        </div>
+      </div>
+
+      <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">
+        <h4 class="mt-1 font-semibold  leading-tight truncate ">
+          2021 l’année du changement
+        </h4>
+        <div class="mt-1 text-sm">
+          Du renouveau et un retour à la terre !
+          Tout à commencez dans le cabanon, au fond de mon jardin ... ⛺
+        </div>
+      </div>
+
+      <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+        <h4 class="mt-1 font-semibold  leading-tight truncate">
+          Une formation en ligne
+        </h4>
+        <div class="mt-1 text-sm">
+          Derniere mon écran 💻
+        </div>
+      </div>
+
+      <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+        <h4 class="mt-1 font-semibold  leading-tight truncate">
+          Une formation en vraie
+        </h4>
+        <div class="mt-1 text-sm">
+          Les mains dedans 🖐
+        </div>
+      </div>
+
+      <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">
+        <h4 class="mt-1 font-semibold  leading-tight truncate ">
+          2021 l’année du changement
+        </h4>
+        <div class="mt-1 text-sm">
+          Du renouveau et un retour à la terre !
+          Tout à commencez dans le cabanon, au fond de mon jardin ... ⛺
+        </div>
+      </div>
+
+      <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+        <h4 class="mt-1 font-semibold  leading-tight truncate">
+          Une formation en ligne
+        </h4>
+        <div class="mt-1 text-sm">
+          Derniere mon écran 💻
+        </div>
+      </div>
+
+      <div class="pl-4 pr-4 pb-4 pt-4 rounded-lg">
+        <h4 class="mt-1 font-semibold  leading-tight truncate">
+          Une formation en vraie
+        </h4>
+        <div class="mt-1 text-sm">
+          Les mains dedans 🖐
+        </div>
+      </div>
+
+      <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">
+        <nuxt-link to="/catalog" class="bg-white hover:bg-gray-100 hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full">Consulter le catalogue</nuxt-link>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    layout: 'static'
+  }
+</script>
+
+<style>
+.crop {
+  width: 180px;
+  height: 180px;
+}
+</style>
