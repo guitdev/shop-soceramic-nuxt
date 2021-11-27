2aee3cad769fd58ace5417c7cbb8d48f0bbab1c9
Guillaume Gojard <ggojard@gmail.com>
2021-11-27 16:36:49 +0100

    bg img

--- starter/pages/index.vue
+++ starter/pages/index.vue
@@ -9,9 +9,13 @@
                 Bienvenue sur le catalogue en ligne SO'ceramic.
             </h3>
             <p class="mt-1 text-sm">
-              Ici je vends une partie de mes produits. Fabrication artisanale française. Tous les produits sont des pièces faconnées à la main en quantité raisonée. SO'ceramic offre une gamme de céramiques épurées et contemporaines.
+              J'expose ici une partie de mes produits.</br>
+              Fabrication artisanale française.</br>
+              Tous les produits sont des pièces faconnées à la main en quantité raisonée. SO'ceramic offre une gamme de céramiques épurées et contemporaines.
             </p>
-            <nuxt-link to="/catalog" class="bg-white hover:bg-gray-100 text-soceramicTxt  hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full">Consulter le catalogue</nuxt-link>
+            <p>
+              <nuxt-link to="/catalog" class="bg-white hover:bg-gray-100 text-soceramicTxt  hover:text-yellow-700 font-semibold py-2 px-4 ml-2 border border-gray-400 rounded-full">Consulter le catalogue</nuxt-link>
+            </p>
           </div>
           <div class="w-full sm:9/12 lg:w-4/12 max-h-full">
             <div class="w-full h-auto">
