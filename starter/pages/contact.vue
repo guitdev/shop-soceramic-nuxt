eef70e0e5198366ce9a56b43100d3a9d9b98d0a9
Guillaume Gojard <ggojard@gmail.com>
2021-11-27 23:17:16 +0100

    modify contact

--- starter/pages/contact.vue
+++ starter/pages/contact.vue
@@ -2,15 +2,16 @@
     <div text-soceramicTxt>
 
       <div class="rounded-lg pl-4 pr-4 pb-4 pt-4">
-        <h1 class="mt-1 font-semibold leading-tight truncate ">
-          SO'ceramic
-        </h1>
+        <div style="display:block;overflow:hidden;position:relative;box-sizing:border-box;margin:0">
+          <img class="w-auto" src="~/assets/soceramiclogo-512x512.png" alt="Workflow">
+        </div>
         <h3 class="mt-1 font-semibold leading-tight truncate ">
           Artisan céramiste
         </h3>
         <h3 class="mt-1 font-semibold leading-tight truncate ">
           Production française & eco-responsable
         </h3>
+
         <div class="mt-1 text-sm">
           Pour me contacter: soceramic21[@]gmail.com
         </div>
